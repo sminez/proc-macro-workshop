74c42889b3cd12f7f72254d25fe2339b32aae91b
IDAM <innes.andersonmorrison@gmail.com>
2020-12-18 09:51:45 +0000

    starting builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,34 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let input_ident = input.ident;
+    let builder_ident = format_ident!("{}Builder", input_ident);
 
-    unimplemented!()
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #input_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
 }
